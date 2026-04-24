f95683be5b2fa4b5a8c113b32f9b15a7ab28215c
Michelle Burton <msmichelleburton@yahoo.com>
2026-04-24 07:42:19 -0500

    add auto fill

--- auto-fill/scripts/script.js
+++ auto-fill/scripts/script.js
@@ -1,1 +1,26 @@
 console.log("JS file loaded");
+
+const grid = document.querySelector('.grid-container');
+const controlsMaxCols = document.querySelector('.max-cols');
+const controlsMaxWidth = document.querySelector('.max-width');
+
+controlsMaxCols.addEventListener('change', e => {
+  const value = e.target.value;
+  grid.style.setProperty('--grid-column-count', value);
+});
+
+controlsMaxWidth.addEventListener('change', e => {
+  const value = e.target.value;
+  grid.style.setProperty('--grid-item--min-width', value + 'px');
+});
+
+// Output width of each grid-item as text.
+const gridItems = document.querySelectorAll('.grid-item');
+const resizeObserver = new ResizeObserver(entries => {
+  for (let entry of entries) {
+    const width = Math.floor(entry.contentBoxSize[0].inlineSize);
+    entry.target.textContent = `${width}px`;
+  }
+});
+
+gridItems.forEach(el => resizeObserver.observe(el));
